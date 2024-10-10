10b766a8d1600bc2f380f0032a6aa2f0dde96566
dr.omartalat <dr.omartalat@gmail.com>
2024-10-10 23:22:49 +0300

    101-safely_get_value.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,6 +6,13 @@ Basic annotations - add
 
 def add(a: float, b: float) -> float:
     """
-    function to add two floats
+    Adds two float numbers.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of the two float numbers.
     """
     return a + b
