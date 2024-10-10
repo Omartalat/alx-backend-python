40806f44adde344d903829bdef1a381e19643cc7
dr.omartalat <dr.omartalat@gmail.com>
2024-10-10 20:31:36 +0300

    mandatory done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+type-annotated function add that takes a float a and a float b
+as arguments and returns their sum as a float.
+"""
+
+
+def add(num1: float, num2: float) -> float:
+    """
+    type-annotated function add
+    """
+    return num1 + num2
---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_kv that takes a string k and
+an int OR float v as arguments and returns a tuple.
+The first element of the tuple is the string k.
+The second element is the square of the int/float v
+and should be annotated as a float.
+"""
+from typing import Dict, Tuple
+
+
+def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+    """type-annotated function to_kv"""
+    val = v * v
+    return (k, val)
+
