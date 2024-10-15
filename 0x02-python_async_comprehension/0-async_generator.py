e94d8da048874336ce518519e02893f90c5eacd6
dr.omartalat <dr.omartalat@gmail.com>
2024-10-15 14:45:28 +0300

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ Functions:
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields random floating-point numbers.
     This coroutine will asynchronously yield a random float between 0 and 10,
